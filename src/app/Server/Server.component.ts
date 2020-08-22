2242bdb1aae6027126ddb63f5ce023170c28d751
Abdelrahman Abdelhameed <abdelrahmanabdelhameed@hotmail.com>
2020-08-22 05:09:35 +0200

    work with component

---
+++ src/app/Server/Server.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: "app-Server",
+    templateUrl:"./Server.component.html",
+    styleUrls: ["./server.component.css"]
+})
+export class ServerComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./Server/Server.component"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ServerComponent]
 })
 export class AppModule { }
